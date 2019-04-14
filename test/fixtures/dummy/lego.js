6b5ecf22c9fc4b08c2b93ab3d642850281b45887
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-04-14 12:39:46 +0530

    Add tests

---
+++ test/fixtures/dummy/lego.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'dummy',
+  inlineSource: true,
+  critical: {
+    inline: true,
+    dimensions: [
+      {
+        height: 800,
+        width: 470
+      },
+      {
+        height: 900,
+        width: 1200
+      }
+    ],
+    penthouse: {
+      timeout: 150000
+    }
+  }
+}
