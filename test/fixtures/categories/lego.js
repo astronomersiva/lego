76bbc7b31234370b53936827a0f5813f0972c6de
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-05-15 21:40:52 +0530

    Make post categorisation more robust

---
+++ test/fixtures/categories/lego.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'name': 'categories',
+  'url': '',
+  'inlineSource': true
+}
---
+++ test/fixtures/no-categories/lego.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'name': 'no-categories',
+  'url': '',
+  'inlineSource': true,
+  'skipDirsInPostUrls': true
+}
