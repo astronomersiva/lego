2790243997c46d58f866096b350fa2f04805f305
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-05-15 21:40:52 +0530

    Make post categorisation more robust

--- test/test.js
+++ test/test.js
@@ -7,6 +7,10 @@ const { execSync } = require('child_process');
 describe('lego', function() {
   this.timeout(60 * 1000);
 
+  afterEach(() => {
+    process.chdir('../../../');
+  });
+
   describe('build', function() {
     it('should build the project with cache disabled', async function() {
       let fixturePath = path.join(process.cwd(), 'test', 'fixtures', 'dummy');
@@ -23,6 +27,9 @@ describe('lego', function() {
 
     it('should build the project with cache enabled', async function() {
       delete process.env.SKIP_CACHE;
+
+      let fixturePath = path.join(process.cwd(), 'test', 'fixtures', 'dummy');
+      process.chdir(fixturePath);
 
       execSync('../../../bin/lego b');
 
@@ -41,10 +48,37 @@ describe('lego', function() {
     it('should build the project with a pre-existing cache', async function() {
       delete process.env.SKIP_CACHE;
 
+      let fixturePath = path.join(process.cwd(), 'test', 'fixtures', 'dummy');
+      process.chdir(fixturePath);
+
       execSync('../../../bin/lego b');
 
       let expectedOutput = JSON.parse(fs.readFileSync('expected-output.json'));
       let actualOutput = fixturify.readSync('build');
+
+      assert.deepStrictEqual(expectedOutput, actualOutput);
+    });
+
+    it('should build the project with categories in posts enabled', async function() {
+      let fixturePath = path.join(process.cwd(), 'test', 'fixtures', 'categories');
+      process.chdir(fixturePath);
+
+      execSync('../../../bin/lego b');
+
+      let actualOutput = fixturify.readSync('build');
+      let expectedOutput = JSON.parse(fs.readFileSync('expected-output.json'));
+
+      assert.deepStrictEqual(expectedOutput, actualOutput);
+    });
+
+    it('should build the project with categories in posts disabled', async function() {
+      let fixturePath = path.join(process.cwd(), 'test', 'fixtures', 'no-categories');
+      process.chdir(fixturePath);
+
+      execSync('../../../bin/lego b');
+
+      let actualOutput = fixturify.readSync('build');
+      let expectedOutput = JSON.parse(fs.readFileSync('expected-output.json'));
 
       assert.deepStrictEqual(expectedOutput, actualOutput);
     });
